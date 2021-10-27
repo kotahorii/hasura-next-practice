f3b4021abddd09f6bcba41736037addffbbbbb31
kotahorii <khorii.micron@gmail.com>
2021-10-27 14:12:51 +0900

    first commit

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+import '@testing-library/jest-dom/extend-expect'
+
+it('Should render title text', () => {
+  render(<Home />)
+  expect(screen.getByText('Next.js!')).toBeInTheDocument()
+})
